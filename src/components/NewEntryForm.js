13b793d8c441cf7ae63d1dedc765ba8440c7a10b
unknown <marius.arnautu@gmail.com>
2019-04-17 10:47:08 +0100

    show form fields one at a time

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -7,8 +7,9 @@ class NewEntryForm extends Component {
   state = {
     sentence: '',
     author: '',
-    country: '',
-    options: countryList().getData()
+    country: 'Afghanistan',
+    options: countryList().getData(),
+    currentInput: 'sentence'
   };
 
   inputChange = e => {
@@ -16,53 +17,90 @@ class NewEntryForm extends Component {
     this.setState({ [name]: value });
   };
 
+  nextClick = e => {
+    e.preventDefault();
+    if (this.state.sentence.length > 0) {
+      this.setState({ currentInput: 'author' });
+    }
+    if (this.state.author.length > 0) {
+      this.setState({ currentInput: 'country' });
+    }
+  };
+
   formSubmit = e => {
     const { sentence, author, country } = this.state;
     const { addEntry } = this.props;
     e.preventDefault();
     addEntry({ sentence, author, country });
-    //this.setState({ entry: {} });
+    this.setState({
+      sentence: '',
+      author: '',
+      country: 'Afghanistan',
+      currentInput: 'sentence'
+    });
   };
 
   renderForm = () => {
-    const { sentence, author, country, options } = this.state;
-    console.log(this.state);
-    console.log(options);
+    const { sentence, author, country, options, currentInput } = this.state;
+    const sentenceInput = (
+      <div className="form-group">
+        <label htmlFor="sentence">Sentence</label>
+        <input
+          value={sentence}
+          onChange={this.inputChange}
+          type="text"
+          name="sentence"
+        />
+        <button onClick={this.nextClick} type="text">
+          Next
+        </button>
+      </div>
+    );
+
+    const authorInput = (
+      <div className="form-group">
+        <label htmlFor="author">Author</label>
+        <input
+          value={author}
+          onChange={this.inputChange}
+          type="text"
+          name="author"
+        />
+        <button onClick={this.nextClick} type="text">
+          Next
+        </button>
+      </div>
+    );
+
+    const countrySelect = (
+      <div className="form-group">
+        <label htmlFor="country">Country</label>
+        <select
+          name="country"
+          value={country}
+          onChange={this.inputChange}
+          className="form-control"
+        >
+          {options.map(option => {
+            return (
+              <option key={option.value} value={option.label}>
+                {option.label}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    );
     return (
       <div>
         <form onSubmit={this.formSubmit}>
           <div>
-            <label htmlFor="sentence">Sentence</label>
-            <input
-              value={sentence}
-              onChange={this.inputChange}
-              type="text"
-              name="sentence"
-            />
-            <br />
-            <label htmlFor="author">Author</label>
-            <input
-              value={author}
-              onChange={this.inputChange}
-              type="text"
-              name="author"
-            />
-            <br />
-            <select
-              name="country"
-              value={country}
-              onChange={this.inputChange}
-              className="form-control"
-            >
-              {options.map(option => {
-                return (
-                  <option key={option.value} value={option.label}>
-                    {option.label}
-                  </option>
-                );
-              })}
-            </select>
-            <br />
+            {currentInput === 'sentence'
+              ? sentenceInput
+              : currentInput === 'author'
+              ? authorInput
+              : countrySelect}
+
             <button type="submit">Save</button>
           </div>
         </form>
@@ -70,9 +108,6 @@ class NewEntryForm extends Component {
     );
   };
 
-  componentWillMount() {
-    this.props.fetchEntries();
-  }
   render() {
     return <div>{this.renderForm()}</div>;
   }
